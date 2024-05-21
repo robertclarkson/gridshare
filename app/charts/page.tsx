d3f29c97f5a0efac2d90cc3e4d9b70b5c8810a59
robertclarkson <rob@robertclarkson.net>
2024-05-21 22:42:59 +1200

    chart updates

--- app/charts/page.tsx
+++ app/charts/page.tsx
@@ -1,11 +1,9 @@
-import { graphQlClient } from "@/lib/client";
 
-import { gql } from "@apollo/client";
+import { Card } from "@nextui-org/react";
 import { PrismaClient } from "@prisma/client";
 import { getServerSession } from "next-auth";
-import MiningChart from "./MiningChart";
-import { Card } from "@nextui-org/react";
 import { authOptions } from "../api/auth/[...nextauth]/authOptions";
+import MiningChart from "./MiningChart";
 
 const prisma = new PrismaClient();
 
@@ -13,7 +11,10 @@ const getUser = async (id: string) => {
     const profile = await prisma.user.findUnique({
         where: { id: id },
 
-        include: { hashing: { orderBy: { date: "asc" } } },
+        include: { 
+            hashing: { orderBy: { date: "asc" } },
+            disposals: { orderBy: { date: "asc" } }
+        },
     });
     return profile;
 };
@@ -35,11 +36,19 @@ export default async function Charts() {
         );
     }
     //Accumulate the chart data.
+
+    const disposals: any[] = [];
+    disposals.push(["date", "BTC/NZD"]);
+
     const coinPrice: any[] = [];
     coinPrice.push(["date", "BTC/NZD"]);
 
     const hashRevenue = [];
-    hashRevenue.push(["date", "Mined BTC", "BTC value in $", "Electricity cost", "Profit"]);
+    hashRevenue.push(["date","BTC value in $", "Electricity cost", "Profit"]);
+
+    const hashRevenueCume = [];
+    hashRevenueCume.push(["date","BTC value in $", "Electricity cost", "Profit"]);
+
     const hashUptime = [];
     hashUptime.push(["date", "uptimeMinutes"]);
     const relativeMining = [];
@@ -62,7 +71,9 @@ export default async function Charts() {
         const profit = bitcoinValue - elect;
         totalProfit = totalProfit + profit;
         totalBitcoinValue += bitcoinValue;
-        hashRevenue.push([new Date(item.date), parseFloat(item.revenue), bitcoinValue, elect, profit]);
+        hashRevenue.push([new Date(item.date), bitcoinValue, elect, profit]);
+
+        hashRevenueCume.push([new Date(item.date), totalBitcoinValue, totalElecCost, totalProfit]);
 
         hashUptime.push([new Date(item.date), parseFloat(item.uptimePercentage)]);
         relativeMining.push([
@@ -71,6 +82,13 @@ export default async function Charts() {
         ]);
         coinPrice.push([new Date(item.time_period_start), item.averagePrice]);
     });
+
+    let totalSales = 0;
+    user?.disposals.forEach((item: any) => {    
+        totalSales+= item.dollars;
+        disposals.push([new Date(item.date), totalSales]);
+    });
+
 
     // console.log(revenueCume)
     // console.log('sumscoremary', score.getHashrateScoreHistory.nodes)
@@ -82,20 +100,34 @@ export default async function Charts() {
                     title: "Mining daily stats",
                     series: {
                         0: { targetAxisIndex: 0 },
-                        1: { targetAxisIndex: 1 },
-                        2: { targetAxisIndex: 1 },
-                        3: { targetAxisIndex: 1 },
+                        1: { targetAxisIndex: 0 },
+                        2: { targetAxisIndex: 0 },
                     },
                     vAxes: {
                         // Adds titles to each axis.
-                        0: { title: "Bitcoin Mined" },
-                        1: { title: "$ NZD" },
+                        0: { title: "$ NZD" },
+                    },
+                }}
+            />
+             <MiningChart
+                data={hashRevenueCume}
+                options={{
+                    title: "Mining cumulative stats",
+                    series: {
+                        0: { targetAxisIndex: 0 },
+                        1: { targetAxisIndex: 0 },
+                        2: { targetAxisIndex: 0 },
+                    },
+                    vAxes: {
+                        // Adds titles to each axis.
+                        0: { title: "$ NZD" },
                     },
                 }}
             />
             <MiningChart data={hashUptime} options={{ title: "Miner Daily Uptime %" }} />
             <MiningChart data={relativeMining} options={{ title: "Bitcoin per unit uptime" }} />
             <MiningChart data={coinPrice} options={{ title: "Bitcoin Price" }} />
+            <MiningChart data={disposals} options={{ title: "Bitcoin Sales" }} />
         </main>
     );
 }
